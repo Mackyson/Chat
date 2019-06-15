5c9cf20624300b01786f35e0d879932398c00a51
Mackyson <mackyson72@gmail.com>
2019-06-16 01:09:14 +0900

    サーバ側の処理

--- server/chat.go
+++ server/chat.go
@@ -36,8 +36,48 @@ func newRoom() *room {
 		clients:  make(map[*client]bool),
 	}
 }
+
+func (r *room) Join(c *client) {
+	r.join <- c
+}
+func (r *room) Leave(c *client) {
+	r.leave <- c
+}
+func (r *room) Receive(msg *string) {
+	r.receive <- msg
+}
+func (r *room) SendPastMessages(c *client) {
+	for _, msg := range r.messages {
+		c.write(msg)
+	}
+}
+func (r *room) Broadcast(msg *string) {
+	for c := range r.clients {
+		c.write(msg)
+	}
+}
+func (r *room) listen() {
+	for {
+		select {
+		case c := <-r.join:
+			fmt.Println("New comer!")
+			r.clients[c] = true
+			r.SendPastMessages(c)
+		case c := <-r.leave:
+			fmt.Println("Bye someone!")
+			delete(r.clients, c)
+			close(c.msgCh)
+		case msg := <-r.receive:
+			fmt.Println("New Message!")
+			r.Broadcast(msg)
+			r.messages = append(r.messages, msg)
+		}
+	}
+}
+
 func main() {
 	room := newRoom()
 	fmt.Println("Listening on 8080")
+	go room.listen()
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
